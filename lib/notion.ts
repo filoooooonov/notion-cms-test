2f2d07d036e518ffbfb1969d807a04241efeb472
Aleksei Filonov <alexfiloonov@gmail.com>
2025-01-04 21:56:28 +0200

    Connect to notion DB

---
+++ lib/notion.ts
@@ -0,0 +1,74 @@
+import { Client } from "@notionhq/client";
+import {
+  BlockObjectResponse,
+  PageObjectResponse,
+  QueryDatabaseParameters,
+  QueryDatabaseResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import React from "react";
+import "server-only";
+
+export const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export const fetchPages = React.cache(async () => {
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID!,
+  });
+
+  const response: QueryDatabaseResponse = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+    filter: {
+      property: "Status",
+      status: {
+        equals: "Live",
+      },
+    },
+  });
+
+  return response.results.map((page) => {
+    const properties = (page as PageObjectResponse).properties;
+    const title =
+      properties.Name.type === "title"
+        ? properties.Name.title[0].plain_text
+        : "Untitled";
+    const date =
+      properties.Date.type === "date" && properties.Date.date
+        ? properties.Date.date.start
+        : "No date";
+    const slug =
+      properties.Slug.type === "rich_text"
+        ? properties.Slug.rich_text[0].plain_text
+        : "404";
+
+    return {
+      id: page.id,
+      title,
+      date,
+      slug,
+    };
+  });
+});
+
+export const fetchBySlug = React.cache((slug: string) => {
+  return notion.databases
+    .query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property: "Slug",
+        rich_text: {
+          equals: slug,
+        },
+      },
+    })
+    .then((res) => res.results[0] as PageObjectResponse | undefined);
+});
+
+export const fetchPageBlocks = React.cache((pageId: string) => {
+  return notion.blocks.children
+    .list({
+      block_id: pageId,
+    })
+    .then((res) => res.results as BlockObjectResponse[]);
+});
